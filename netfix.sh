388c3f613a1b8fb07ee08300ec5410d751904dd8
Gaston Bezzi <gaston@thousandeyes.com>
2017-08-10 11:55:10 +0100

    More automation

--- netfix.sh
+++ netfix.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env sh
-
-####################################################################################
-# This script is a failsafe to be used in puppet                                   #
-# It is used to prevent being kicked from a server due to wrong network configs    #
-# It assumes that a backup for the interfaces file was made and restores it before #
-# rebooting the server if there's no internet connectivity                         #
-####################################################################################
-
 #set -x
 
-# Parameters
 SLEEP_SECONDS=2
 MAX_ITERATIONS=10
 PING_WAIT=4
 PING_IP='8.8.8.8'
 IF_BACKUP_FILE='/etc/network/interfaces.bak'
-
+GRUB_FILE='/etc/default/grub.bak'
 count=0
+
 while [ $count -lt $MAX_ITERATIONS ]; do
   /bin/ping -c 1 -W $PING_WAIT $PING_IP >/dev/null
   rc=$?
@@ -45,4 +37,4 @@ if [ $rc -ge 1 ]; then
   else
     exit 0
   fi
-fi
+fi 
--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-!#/bin/bash
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
@@ -7,10 +11,21 @@ fi
 SMBIOS_V=$(dmidecode -t 0 | grep SMBIOS | tail -1 | awk '{print $2}')
 if [ $(echo "$SMBIOS_V<2.6"| bc ) == 0 ]
 then
-  printf "GOOD NEWS: You can change interface naming with biosdevname. \n"
+  printf "${GREEN}GOOD NEWS:${NC} You can change interface naming with biosdevname. \n"
 else
-  printf "BAD NEWS: You can not change interface naming with biosdevname. \n"
+  printf "${RED}BAD NEWS:${NC} You can not change interface naming with biosdevname. Create some ${RED}RULES${NC} !! \n"
   exit 0
+fi
+
+printf "Checking for needed packets ..\n"
+sleep 1;
+if [ $(dpkg-query -W -f='${Status}' bc 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install -y bc;
+fi
+if [ $(dpkg-query -W -f='${Status}' tee 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install -y bc;
 fi
 
 printf "Cheking for udev rules.. \n"
@@ -20,19 +35,45 @@ then
   select yn in "Yes" "No"; do
     case $yn in
         Yes ) rm /etc/udev/rules.d/* ; break;;
-        No ) ;;
+        No )  exit 0;;
     esac
   done
 else
   printf "No rules present. \n"
 fi
 
-printf "Generating netfix script .. \n"
-cp netfix.sh  /usr/local/bin/netfix.sh
+printf "Checking biosdevname packet..\n"
+sleep 2;
+if [ $(dpkg-query -W -f='${Status}' biosdevname 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install -y biosdevname;
+fi
+printf "Package installed\n"
+
+DIFACE=`route -n | grep ^0.0.0.0 | sed 's/  */ /g' | cut -d' ' -f8`
+NIFACE=`biosdevname -i $DIFACE`
+printf "Default interface ${RED}$DIFACE${NC} will be re-named to ${GREEN}$NIFACE${NC} \n"
+sleep 2;
+
 printf "Backing up interfaces file .. \n"
 cp /etc/network/interfaces /etc/network/interfaces.bak
+printf "Updating /etc/network/interfaces file .. \n"
+sed "s/$DIFACE/$NIFACE/g" /etc/network/interfaces | tee  /etc/network/intefaces
+sleep 2;
+
+
+printf "Backing up grub.cfg .. \n"
+cp /etc/default/grub /etc/default/grub.bak
+sleep 1;
+printf "Updating grub configuration .. \n"
+sed '0,/GRUB_CMDLINE_LINUX_DEFAULT/{s/.*GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT="biosdevname=1"/}' /etc/default/grub | tee /etc/default/grub 
+update-grub
+
+printf "Generating ${RED}NETFIX${NC} script .. \n"
+sleep 2;
+cp netfix.sh  /usr/local/bin/netfix.sh
 printf "Enabling netfix service .. \n"
 cp netfix.service /etc/systemd/system/netfix.service
 systemctl enable netfix.service
 printf "Done \n"
-printf "You should: \n 1- Edit /etc/default/grub biosdevflag=1 \n 2- update-grub \n 3- Edit /etc/network/interfaces \n 4- reboot \n :)"
+printf "You should: \n 1- Check \n 2- reboot \n 3- Disable netfix service. /n :) \n"
